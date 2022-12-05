4288f018108766aa46edf7ae8ed267b9e89ec536
Tomasz Rafalski <tomekrafalski1@gmail.com>
2022-12-05 11:31:30 +0100

    AoC 2021 day3 answers

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day3 extends AoCPuzzle {
 
@@ -40,6 +41,50 @@ public class Day3 extends AoCPuzzle {
 
         System.out.println("Day 3 First Answer: " + result);
 
+        String secondResult = "";
+        String thirdResult = "";
+        List<String> OGR = List.copyOf(input);
+        List<String> CSR = List.copyOf(input);
+        for (int i = 0; i < binary.length(); i++) {
+            if (OGR.size() == 1) {
+                secondResult = OGR.get(0);
+            }
+            if (CSR.size() == 1) {
+                thirdResult = CSR.get(0);
+            }
 
+            List<String> zeros;
+            List<String> ones;
+            List<String> zeros1;
+            List<String> ones1;
+            int finalI = i;
+            zeros = OGR.stream().filter(item -> item.charAt(finalI) == '0').collect(Collectors.toList());
+            ones = OGR.stream().filter(item -> item.charAt(finalI) == '1').collect(Collectors.toList());
+            zeros1 = CSR.stream().filter(item -> item.charAt(finalI) == '0').collect(Collectors.toList());
+            ones1 = CSR.stream().filter(item -> item.charAt(finalI) == '1').collect(Collectors.toList());
+            if (zeros.size() > ones.size()) {
+                OGR = zeros;
+            } else {
+                OGR = ones;
+            }
+            if (zeros1.size() > ones1.size()) {
+                CSR = ones1;
+            } else {
+                CSR = zeros1;
+            }
+        }
+        if (OGR.size() == 1) {
+            secondResult = OGR.get(0);
+        }
+        if (CSR.size() == 1) {
+            thirdResult = CSR.get(0);
+        }
+
+
+        gammaRate = Integer.parseInt(secondResult, 2);
+        epsilonRate = Integer.parseInt(thirdResult, 2);
+        result = gammaRate * epsilonRate;
+
+        System.out.println("Day 3 Second Answer: " + result);
     }
 }
